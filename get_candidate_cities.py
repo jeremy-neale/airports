ebc7e0d1bae3e71194462b35b329773e28cd03c7
tikkikkit21 <tikkikkit21@gmail.com>
2024-12-15 19:55:51 -0500

    Move akhil's stuff to root

--- get_candidate_cities.py
+++ get_candidate_cities.py
@@ -1,21 +1,21 @@
 # including modules
 import matplotlib.pyplot as plt
 import pandas as pd
-from utils import test_functions_inside
+from utils import candidate_helper_functions
 
 # Dometics airport details
 print('Loading in datasets...')
-airports_df = pd.read_csv("../dataset/airport_info.csv")
+airports_df = pd.read_csv("dataset/airport_info.csv")
 airports_df = airports_df.drop_duplicates(subset='AIRPORT_ID', keep='first')
 airports_df = airports_df.set_index('AIRPORT_ID')
 
 # Dometics airlines connections
-trips_df = pd.read_csv("../dataset/flights.csv")
-cities_df = pd.read_csv("../dataset/uscities.csv")
+trips_df = pd.read_csv("dataset/flights.csv")
+cities_df = pd.read_csv("dataset/uscities.csv")
 
 # Calculate airport size based on number of connections
 print('Calculating airport sizes...')
-airports_df = test_functions_inside.determine_airport_size(airports_df, trips_df)
+airports_df = candidate_helper_functions.determine_airport_size(airports_df, trips_df)
 
 # Sort and filter results
 print('Sorting and filtering...')
@@ -25,7 +25,7 @@ airports_df = airports_df.loc[airports_df['num_connections'] > 100]
 # Rank cities
 print('Final length:', len(airports_df))
 print('Ranking cities for new airports...')
-cities = test_functions_inside.rank_cities_for_new_airports(cities_df, airports_df, 100000, 100)
+cities = candidate_helper_functions.rank_cities_for_new_airports(cities_df, airports_df, 100000, 100)
 
 # Export
 print('Exporting to candidate_cities.csv')
